03536fe278425c8616140d57cb5e899894bbfa10
hdrcruz <hdr.cruz@gmail.com>
2016-04-06 21:33:20 -0300

    Arquivos aula 3 - arvores binarias

--- Aula3/Pessoa.hpp
+++ Aula3/Pessoa.hpp
@@ -9,26 +9,35 @@ class Pessoa
 
 	protected:
 		string nome;
+		int idade;
 
 
 	private:
+		Pessoa *filho_esquerda;
+		Pessoa *filho_direita;
 
 
 	public:
 		Pessoa()
 		{
 			nome = "";
+			idade = 0;
+			filho_esquerda == NULL;
+			filho_direita == NULL;
 		};
 
 
-		Pessoa(string nm)
+		Pessoa(string nm, int ida)
 		{
 			nome = nm;
+			idade = ida;
+			filho_esquerda = NULL;
+			filho_direita = NULL;
 		};
 
 		~Pessoa()
 		{
-			cout << "Deletando instancia de Pessoa" << endl;
+			cout << "Deletando instancia de Pessoa " << this->nome << endl;
 		};
 
 		string getNome()
@@ -40,4 +49,89 @@ class Pessoa
 		{
 			nome = nm;
 		};
+
+
+		int getIdade()
+		{
+			return idade;
+		};
+
+		void setIdade(int valor)
+		{
+			idade = valor;
+		};
+
+		void setFilhoEsquerda(Pessoa *pessoa){
+			this->filho_esquerda = pessoa;
+		};
+
+		void setFilhoDireita(Pessoa *pessoa){
+			this->filho_direita = pessoa;
+		};
+
+		Pessoa* getFilhoEsquerda(){
+			return this->filho_esquerda;
+		};
+
+		Pessoa* getFilhoDireita(){
+			return this->filho_direita;
+		};
+
+		int inserirFilho(Pessoa *filho) {
+			if (filho->getIdade() < this->getIdade()) {
+				if(this->filho_esquerda==NULL){
+					this->filho_esquerda = filho;
+					return 1;
+				}
+				else{
+					this->filho_esquerda->inserirFilho(filho);
+				}
+			}
+			else{
+				if (this->filho_direita==NULL) {
+					this->filho_direita = filho;
+					return 1;
+				}
+				else {
+					this->filho_direita->inserirFilho(filho);
+				}
+			}
+		};
+
+		int listarPre(){
+			cout << this->getNome() << endl;
+			if ((this->filho_esquerda == NULL) && (this->filho_direita==NULL)) {
+				return 0;
+			}
+			else{
+				if (this->filho_esquerda != NULL) {
+					this->filho_esquerda->listarPre();
+				}
+				if (this->filho_direita != NULL) {
+					this->filho_direita->listarPre();
+				}
+
+			}
+		};
+
+
+		int apagar(){
+			if ((this->filho_esquerda == NULL) && (this->filho_direita==NULL)) {
+				delete this;
+				return 0;
+			}
+			else{
+				if (this->filho_esquerda != NULL) {
+					this->filho_esquerda->apagar();
+				}
+				if (this->filho_direita != NULL) {
+					this->filho_direita->apagar();
+				}
+				delete this;
+			}
+
+		}
+
+
+
 };
--- Aula3/main.cpp
+++ Aula3/main.cpp
@@ -3,35 +3,54 @@
 #include "PessoaJuridica.hpp"
 
 int main(int argc, char **argv){
+  bool continuar = true;
+  int idade;
+  string nome;
+  int i;
 
-    string nome;
-    int cpf;
-    int cnpj;
+  Pessoa *raiz = NULL;
 
-    PessoaFisica fisica;
-	cout << "Informe o nome:" << endl;
-	cin >> nome;
-	fisica.setNome(nome);
+  //Pessoa *p1 = new Pessoa("Pessoa1",20);
+  //Pessoa *p2 = new Pessoa("Pessoa2",10);
+  //Pessoa *lista_pessoas;
+  //lista_pessoas = new Pessoa[5];
 
-    cout << "Informe o cpf:" << endl;
-	cin >> cpf;
-	fisica.setCpf(cpf);
+  for (i = 0; i < 5; i++) {
+    cout << "Inserir Nome: " << endl;
+    cin >> nome;
+    cout << "Inserir Idade: " << endl;
+    cin >> idade;
+    //lista_pessoas[i].setNome(nome);
+    //lista_pessoas[i].setIdade(idade);
+    Pessoa *pessoa = new Pessoa(nome, idade);
+    if (raiz == NULL) {
+      raiz = pessoa;
+    }
+    else{
+      raiz->inserirFilho(pessoa);
+    }
+  }
 
-	cout << endl << "NOME: " << fisica.getNome() << endl;
-	cout << "CPF: " << fisica.getCpf() << endl;
+  raiz->listarPre();
 
-    PessoaJuridica juridica;
-	cout << endl << "Informe o nome:" << endl;
-	cin >> nome;
-	juridica.setNome(nome);
-
-    cout << "Informe o cnpj:" << endl;
-	cin >> cnpj;
-	juridica.setCpf(cnpj);
-
-	cout << endl << "NOME: " << juridica.getNome() << endl;
-	cout << "CNPJ: " << juridica.getCpf() << endl;
+  raiz->apagar();
 
 
+
+
+
+
+  //raiz = p1;
+  //cout << "Inseriu esquerda " << p1->inserirFilho(p2) << endl;
+  //cout << "Filho esquerdo: " << raiz->getFilhoEsquerda()->getNome() << endl;
+  // cout << "Filho esquerdo: " << raiz->getFilhoEsquerda() << endl;
+  // cout << "Filho esquerdo: " << raiz->getFilhoDireita() << endl;
+
+
+
+  //delete p1;
+  //delete p2;
+  //delete [] lista_pessoas;
+  //delete raiz;
 return 0;
 }
