f477ffeb1af74743fe784daf4893c942b56bf029
hdrcruz <hdr.cruz@gmail.com>
2016-04-13 21:46:40 -0300

    falta exclusão terceiro caso

--- Aula3/Pessoa.hpp
+++ Aula3/Pessoa.hpp
@@ -15,6 +15,7 @@ class Pessoa
 	private:
 		Pessoa *filho_esquerda;
 		Pessoa *filho_direita;
+		Pessoa *pai;
 
 
 	public:
@@ -22,8 +23,9 @@ class Pessoa
 		{
 			nome = "";
 			idade = 0;
-			filho_esquerda == NULL;
-			filho_direita == NULL;
+			filho_esquerda = NULL;
+			filho_direita = NULL;
+			pai = NULL;
 		};
 
 
@@ -33,6 +35,7 @@ class Pessoa
 			idade = ida;
 			filho_esquerda = NULL;
 			filho_direita = NULL;
+			pai = NULL;
 		};
 
 		~Pessoa()
@@ -69,32 +72,38 @@ class Pessoa
 			this->filho_direita = pessoa;
 		};
 
-		Pessoa* getFilhoEsquerda(){
-			return this->filho_esquerda;
+		void setPai(Pessoa *pessoa){
+			this->pai = pessoa;
+		};
+
+		Pessoa* getPai(){
+			return this->pai;
 		};
 
 		Pessoa* getFilhoDireita(){
 			return this->filho_direita;
 		};
 
+		Pessoa* getFilhoEsquerda(){
+			return this->filho_esquerda;
+		};
+
 		int inserirFilho(Pessoa *filho) {
-			if (filho->getIdade() < this->getIdade()) {
+			if (filho->getIdade() < this->idade ){
 				if(this->filho_esquerda==NULL){
 					this->filho_esquerda = filho;
+					filho->setPai(this);
 					return 1;
 				}
-				else{
-					this->filho_esquerda->inserirFilho(filho);
-				}
+				else this->filho_esquerda->inserirFilho(filho);
 			}
 			else{
 				if (this->filho_direita==NULL) {
 					this->filho_direita = filho;
+					filho->setPai(this);
 					return 1;
 				}
-				else {
-					this->filho_direita->inserirFilho(filho);
-				}
+				else this->filho_direita->inserirFilho(filho);
 			}
 		};
 
@@ -104,33 +113,75 @@ class Pessoa
 				return 0;
 			}
 			else{
-				if (this->filho_esquerda != NULL) {
-					this->filho_esquerda->listarPre();
-				}
-				if (this->filho_direita != NULL) {
-					this->filho_direita->listarPre();
-				}
-
+				if (this->filho_esquerda != NULL) this->filho_esquerda->listarPre();
+				if (this->filho_direita != NULL) this->filho_direita->listarPre();
 			}
 		};
 
 
-		int apagar(){
+		int apagarArvore(){
 			if ((this->filho_esquerda == NULL) && (this->filho_direita==NULL)) {
 				delete this;
 				return 0;
 			}
 			else{
-				if (this->filho_esquerda != NULL) {
-					this->filho_esquerda->apagar();
-				}
-				if (this->filho_direita != NULL) {
-					this->filho_direita->apagar();
-				}
+				if (this->filho_esquerda != NULL) 	this->filho_esquerda->apagarArvore();
+				if (this->filho_direita != NULL) this->filho_direita->apagarArvore();
 				delete this;
 			}
-
 		}
+
+		Pessoa* buscar(int valor){
+			cout << "Nome Atual:" << this->nome << " Idade: " << this->idade << " Busca: " << valor << endl;
+			if(this->idade == valor) return this;
+			if ((this->idade < valor) && (this->filho_direita!=NULL))  return this->filho_direita->buscar(valor);
+			if (this->filho_esquerda!=NULL)   return this->filho_esquerda->buscar(valor);
+			return NULL;
+		};
+
+		int apagar(int valor){
+			Pessoa *apagar = this->buscar(valor);
+			if(apagar->getPai() == NULL){
+				cout << "Apaga raiz implementar mais tarde!" << endl;
+				return 1;
+			}
+			if (apagar->isFolha()) {
+				if (apagar->pai->getFilhoDireita() == apagar) apagar->pai->setFilhoDireita(NULL);
+				else apagar->pai->setFilhoEsquerda(NULL);
+				delete apagar;
+				return 0;
+			}
+			if (apagar->getFilhoDireita() != NULL){
+				if (apagar->getFilhoDireita()->isFolha()){
+					if (apagar->pai->getFilhoDireita() == apagar) apagar->pai->setFilhoDireita(apagar->getFilhoDireita());
+					else apagar->pai->setFilhoEsquerda(apagar->getFilhoDireita());
+					apagar->filho_direita->setPai(apagar->getPai());
+					delete apagar;
+					return 0;
+				}
+			}
+			if (apagar->getFilhoEsquerda() != NULL){
+				if (apagar->getFilhoEsquerda()->isFolha()){
+					if (apagar->pai->getFilhoDireita() == apagar) apagar->pai->setFilhoDireita(apagar->getFilhoEsquerda());
+					else apagar->pai->setFilhoEsquerda(apagar->getFilhoEsquerda());
+					apagar->filho_esquerda->setPai(apagar->getPai());
+					delete apagar;
+					return 0;
+				}
+			}
+			return 1;
+
+		};
+
+		bool isFolha(){
+			return (this->getFilhoDireita() == NULL) && (this->getFilhoEsquerda()==NULL);
+		};
+
+		bool isFull(){
+			return (this->getFilhoDireita() != NULL) && (this->getFilhoEsquerda()!=NULL);
+		};
+
+
 
 
 
--- Aula3/main.cpp
+++ Aula3/main.cpp
@@ -1,6 +1,6 @@
 #include "Pessoa.hpp"
-#include "PessoaFisica.hpp"
-#include "PessoaJuridica.hpp"
+//#include "PessoaFisica.hpp"
+//#include "PessoaJuridica.hpp"
 
 int main(int argc, char **argv){
   bool continuar = true;
@@ -10,18 +10,11 @@ int main(int argc, char **argv){
 
   Pessoa *raiz = NULL;
 
-  //Pessoa *p1 = new Pessoa("Pessoa1",20);
-  //Pessoa *p2 = new Pessoa("Pessoa2",10);
-  //Pessoa *lista_pessoas;
-  //lista_pessoas = new Pessoa[5];
-
   for (i = 0; i < 5; i++) {
     cout << "Inserir Nome: " << endl;
     cin >> nome;
     cout << "Inserir Idade: " << endl;
     cin >> idade;
-    //lista_pessoas[i].setNome(nome);
-    //lista_pessoas[i].setIdade(idade);
     Pessoa *pessoa = new Pessoa(nome, idade);
     if (raiz == NULL) {
       raiz = pessoa;
@@ -31,26 +24,22 @@ int main(int argc, char **argv){
     }
   }
 
+  cout << "Arvore antes da exclusão!" << endl;
   raiz->listarPre();
 
-  raiz->apagar();
+  cout << "Inserir Idade para buscar: " << endl;
+  cin >> idade;
+
+  //cout << raiz->buscar(idade) << endl;
+
+  raiz->apagar(idade);
+
+  cout << "Arvore depois da exclusão!" << endl;
+  raiz->listarPre();
 
 
+  cout << "Apagando Arvore geral!" << endl;
+  raiz->apagarArvore();
 
-
-
-
-  //raiz = p1;
-  //cout << "Inseriu esquerda " << p1->inserirFilho(p2) << endl;
-  //cout << "Filho esquerdo: " << raiz->getFilhoEsquerda()->getNome() << endl;
-  // cout << "Filho esquerdo: " << raiz->getFilhoEsquerda() << endl;
-  // cout << "Filho esquerdo: " << raiz->getFilhoDireita() << endl;
-
-
-
-  //delete p1;
-  //delete p2;
-  //delete [] lista_pessoas;
-  //delete raiz;
 return 0;
 }
